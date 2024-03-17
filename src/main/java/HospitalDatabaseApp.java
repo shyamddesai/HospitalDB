f770394b7ebce569c59e62d6b3d223ebdf874255
shyamdesai03 <shyam.desai@mail.mcgill.ca>
2024-03-17 15:15:50 -0400

    List of Equipment and Staff

--- src/main/java/HospitalDatabaseApp.java
+++ src/main/java/HospitalDatabaseApp.java
@@ -29,11 +29,13 @@ public class HospitalDatabaseApp {
         while (!exit) {
             System.out.println("\n======== Hospital Menu ========");
             System.out.println("1. View Patient Prescriptions");
-            System.out.println("2. View Equipment Inventory");
-            System.out.println("3. Add New Equipment");
-            System.out.println("4. Schedule an Appointment");
-            System.out.println("5. Cancel an Appointment");
-            System.out.println("6. Update Patient Records");
+            System.out.println("2. View Hospital Equipment and Staff");
+            System.out.println("3.  ");
+            System.out.println("4. View Equipment Inventory");
+            System.out.println("5. Add New Equipment");
+            System.out.println("6. Schedule an Appointment");
+            System.out.println("7. Cancel an Appointment");
+            System.out.println("8. Update Patient Records");
             System.out.println("0. Quit");
             System.out.print("Please Enter Your Option: ");
 
@@ -42,19 +44,25 @@ public class HospitalDatabaseApp {
             switch (option) {
                 case 1:
                     viewPatientPrescriptions(conn);
+                    break;
                 case 2:
-                    viewEquipmentInventory(conn);
+                    viewHospitalEquipmentAndStaff(conn);
                     break;
                 case 3:
-                    addNewEquipment(conn, scanner);
                     break;
                 case 4:
-                    scheduleAppointment(conn, scanner);
+                    viewEquipmentInventory(conn);
                     break;
                 case 5:
-                    cancelAppointment(conn, scanner);
+                    addNewEquipment(conn, scanner);
                     break;
                 case 6:
+                    scheduleAppointment(conn, scanner);
+                    break;
+                case 7:
+                    cancelAppointment(conn, scanner);
+                    break;
+                case 8:
                     updatePatientRecords(conn, scanner);
                     break;
                 case 0:
@@ -115,6 +123,55 @@ public class HospitalDatabaseApp {
         }
     }
 
+    private static void viewHospitalEquipmentAndStaff(Connection conn) {
+        String query = "WITH HospitalEquipment AS ("
+                + "SELECT h.hname, h.haddress, e.equipment_type, "
+                + "AVG(e.life_expetancy) AS AvgLifeExpectancy "
+                + "FROM equipment e "
+                + "JOIN hospital h "
+                + "ON e.hname = h.hname "
+                + "AND e.haddress = h.haddress "
+                + "GROUP BY h.hname, h.haddress, e.equipment_type "
+                + "), "
+                + "PersonnelCount AS ("
+                + "SELECT hname, haddress, COUNT(employee_No) AS TotalPersonnel "
+                + "FROM personnel "
+                + "GROUP BY hname, haddress "
+                + ") "
+                + "SELECT "
+                + "he.hname AS Hospital, "
+                + "he.haddress AS \"Hospital Address\", "
+                + "he.equipment_type AS \"Equipment Type\", "
+                + "he.AvgLifeExpectancy AS \"Equipment Avg. Life Expectancy\", "
+                + "pc.TotalPersonnel AS \"Total Staff\" "
+                + "FROM HospitalEquipment he "
+                + "JOIN PersonnelCount pc "
+                + "ON he.hname = pc.hname "
+                + "AND he.haddress = pc.haddress "
+                + "ORDER BY he.hname, he.equipment_type;";
+
+        try (Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            System.out.println(String.format("%-30s%-30s%-20s%-35s%-15s",
+                    "Hospital", "Hospital Address", "Equipment Type",
+                    "Equipment Avg. Life Expectancy", "Total Staff"));
+
+            while (rs.next()) {
+                String hospital = rs.getString("Hospital");
+                String hospitalAddress = rs.getString("Hospital Address");
+                String equipmentType = rs.getString("Equipment Type");
+                double avgLifeExpectancy = rs.getDouble("Equipment Avg. Life Expectancy");
+                int totalStaff = rs.getInt("Total Staff");
+
+                System.out.println(String.format("%-30s%-30s%-20s%-35f%-15d",
+                        hospital, hospitalAddress, equipmentType,
+                        avgLifeExpectancy, totalStaff));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching hospital equipment and staff information: " + e.getMessage());
+        }
+    }
 
     private static void viewEquipmentInventory(Connection conn) {
         // Implementation of viewing equipment inventory
